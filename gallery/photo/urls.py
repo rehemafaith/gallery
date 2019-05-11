235944700624b5598924d0eb9b543a38650b27e6
rehemafaith <rehemafaith01@gmail.com>
2019-05-12 01:37:51 +0300

    added templates to the app

--- gallery/photo/urls.py
+++ gallery/photo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="photo-home"),
+    path('about/',views.about, name="photo-about"),
 ]
--- gallery/photo/views.py
+++ gallery/photo/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>hi</h1>')
+    return render(request,'photo/photos.html')
+
+
+def about(request):
+    return HttpResponse('photo/zoom.html')  
