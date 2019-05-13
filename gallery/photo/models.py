1a30e0d62785a4663e516f1f438a85b3bcc54d15
rehemafaith <rehemafaith01@gmail.com>
2019-05-13 10:20:00 +0300

    added models

--- gallery/photo/models.py
+++ gallery/photo/models.py
@@ -16,7 +16,7 @@ class Location(models.Model):
     self.delete()
 
   def update(self,field,val):
-    Location.objects.get(id = self.id).update(field = val)
+    Location.objects.filter(id = self.id).update(field = val)
 
   def __str__(self):
       return self.name
@@ -39,47 +39,60 @@ class Category(models.Model):
   def __str__(self):
       return self.name
 
-  class Image(models.Model):
-    image_url = models.ImageField(upload_to ="images/")
-    name = models.CharField(max_length =30)
-    description = models.CharField
-    location = models.ForeignKey('Location', on_delete=models.CASCADE)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+class Image(models.Model):
+  
+    photo_image = models.ImageField(upload_to = "images/")
+    name = models.CharField(max_length = 30)
+    description = models.TextField()
+    location = models.ForeignKey('Location',on_delete=models.deletion.CASCADE,)
+    category = models.ForeignKey('Category',on_delete=models.deletion.CASCADE,)
 
     def save_image(self):
-
-      self.save()
+        """
+        This is the function that we will use to save the instance of this class
+        """
+        self.save()
 
     def delete_image(self):
-
-      Image.objects.get(id =self.id).delete()
+        """
+        This is the function that we will use to delete the instance of this class
+        """
+        Image.objects.get(id = self.id).delete()
 
     def update_image(self,val):
-
-      Image.objects.filter(id = self.id).update(name = val)
+        """
+        Thisnews/models.py is the method to update the instance
+        """
+        Image.objects.filter(id = self.id).update(name = val)
 
     @classmethod
     def get_image_by_id(cls,image_id):
-
-      return cls.objects.get(id = image_id) 
+        """
+        This is the method to get a specific image
+        """
+        return cls.objects.get(id = image_id)
 
     @classmethod
     def get_images(cls):
-        return cls.objects.all() 
+        return cls.objects.all()
 
     @classmethod
-    def search_image(cls,category):
-      try:
-        searched_category = Category.objects.filter(name_icontains = category)[0]
-        return cls.object.filter(category_id =searched_category.id)
-
-      except Exeption:
-        return "No images found"
-
+    def search_by_category(cls,category):
+        """
+        This is the method to search images based on a specific category
+        """
+           
+        searched_category = Category.objects.filter(name__icontains  = category)[0]
+        return cls.objects.filter(category_id = searched_category.id)
+        
+        
     @classmethod
     def filter_by_location(cls,location):
-        the_location = Loction.objects.get(name = location)
-        return cls.objects.fiter(location_id = the_location.id)
+        """
+        This is the method to get images taken in a certain location
+        """
+        the_location = Location.objects.get(name = location)
+        return cls.objects.filter(location_id = the_location.id)
 
     def __str__(self):
-        return self.name 
+        return self.name
