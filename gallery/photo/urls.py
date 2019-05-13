7bcdbaef423ff8b895b46a026c5d2254e0dcd22f
rehemafaith <rehemafaith01@gmail.com>
2019-05-13 10:21:42 +0300

    added links to the URLConf

--- gallery/photo/urls.py
+++ gallery/photo/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings 
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('',views.home, name="photo-home"),
-    path('about/',views.about, name="photo-about"),
+    
+    path('search/', views.search_results, name='search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
