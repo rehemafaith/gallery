0b580b03d2dc15eb3d7991564598d5070f0cea48
rehemafaith <rehemafaith01@gmail.com>
2019-05-13 10:20:32 +0300

    added content for the admin

--- gallery/photo/admin.py
+++ gallery/photo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Location,Category,Image
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+  filter_horizontal=('Category',)
+
+
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
